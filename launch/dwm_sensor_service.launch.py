14e1b00adb2e1b31d2539abff41af148419619ef
PhilAzeredo <43647055+PhilAzeredo@users.noreply.github.com>
2022-11-07 10:53:45 -0500

    update sensor service launch file
    Update sensor service launch file to only launch one node under the default node name

--- launch/dwm_sensor_service.launch.py
+++ launch/dwm_sensor_service.launch.py
@@ -9,23 +9,11 @@ def generate_launch_description():
         'config',
         'multi_ips.yaml'
     )
-    rear_ips_config = os.path.join(
-        get_package_share_directory('dwm1001_ros2'),
-        'config',
-        'rear_ips.yaml'
-    )
 
     return LaunchDescription([
         Node(
             package='dwm1001_ros2',
             executable='dwm1001',
-            name='front_dwm1001',
-            parameters=[multi_ips_config]
-        ),
-        Node(
-            package='dwm1001_ros2',
-            executable='dwm1001',
-            name='rear_dwm1001',
             parameters=[multi_ips_config]
         )
     ])
